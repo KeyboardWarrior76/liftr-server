4abb05930549def9b3870c20e89e5c9768d8813a
3mpirical <3mpirical10510@gmail.com>
2019-07-17 20:11:31 -0600

    added the create method to rep_scheme controller

--- app/controllers/api/rep_schemes_controller.rb
+++ app/controllers/api/rep_schemes_controller.rb
@@ -1,5 +1,6 @@
 class Api::RepSchemesController < ApplicationController
     before_action :set_training_date
+    before_action :set_exercise, only: [:create]
 
     def index
         render( json: @training_date.rep_schemes.all() )
@@ -10,7 +11,16 @@ class Api::RepSchemesController < ApplicationController
     end
 
     def create
+        rep_scheme = @training_date.rep_schemes.new(
+            exercise_id: @exercise.id,
+            exercise_name: @exercise.name,
+        )
 
+        if rep_scheme.save()
+            render( json: rep_scheme, status: 201 )
+        else
+            render( json: { error: rep_scheme.errors, message: "Could Not Create Data" }, status: 422 )
+        end
     end
 
     def update
@@ -28,5 +38,9 @@ class Api::RepSchemesController < ApplicationController
 
         def set_training_date
             @training_date = TrainingDate.find(params[:training_date_id])
+        end
+
+        def set_exercise
+            @exercise = Exercise.find(params[:exercise_id])
         end
 end
