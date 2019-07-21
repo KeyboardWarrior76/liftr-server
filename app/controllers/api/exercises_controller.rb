ba4441ac54350db3dd482cba30fe6258a1086acd
3mpirical <3mpirical10510@gmail.com>
2019-07-20 22:43:41 -0600

    updated exercises controller to accept kind param from request

--- app/controllers/api/exercises_controller.rb
+++ app/controllers/api/exercises_controller.rb
@@ -1,5 +1,5 @@
 class Api::ExercisesController < ApplicationController
     def search
-        render( json: Exercise.search(params[:term], params[:body_part]) )
+        render( json: Exercise.search(params[:term], params[:body_part], params[:kind]) )
     end
 end
