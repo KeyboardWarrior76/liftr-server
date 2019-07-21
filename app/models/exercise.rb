e792efb888a609e252086c10d82f23d3882bbd1c
3mpirical <3mpirical10510@gmail.com>
2019-07-20 19:10:07 -0600

    updated Exercise.search to take a body_part and kind argument, and filter results

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,11 +1,34 @@
 class Exercise < ApplicationRecord
     has_many :rep_schemes
 
-    def self.search(term)
+    def self.search(term, body_part=nil, kind=nil)
+        allowed_body_parts = {
+            "back" => true, 
+            "chest" => true, 
+            "shoulders" => true, 
+            "legs" => true, 
+            "arms" => true,
+            "abs" => true,
+            "cardio" => true,
+        }
+
+        allowed_kinds = {
+            "weight" => true,
+            "distance" => true,
+        }
+
+        term = (term == " " ? "%" : "%#{term}%")
+        body_part = "%" if(!allowed_body_parts[body_part])
+        kind = "%" if (!allowed_kinds[kind])
+
+
         Exercise.find_by_sql(["
             SELECT * FROM exercises
             WHERE name ILIKE ?
+            AND main_bodypart LIKE ?
+            AND kind LIKE ?
             ORDER BY name
-            ", term == " " ? "%" : "%#{term}%"])
+            ", 
+            term, body_part, kind])
     end
 end
