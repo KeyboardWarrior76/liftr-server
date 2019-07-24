abca0e2e37ef294ebeab703ee2c7acd73512aaf8
3mpirical <3mpirical10510@gmail.com>
2019-07-24 16:03:09 -0600

    added authenticate_user! function to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
     include DeviseTokenAuth::Concerns::SetUserByToken
-#     before_action :authenticate_user!, if: proc { request.controller_class.parent == Api }
+    before_action :authenticate_user!, if: proc { request.controller_class.parent == Api }
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
